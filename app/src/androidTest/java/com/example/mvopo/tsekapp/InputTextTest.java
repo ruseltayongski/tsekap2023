8fc4a119eea7b7289ef5ab7936a302c10a600d0b
Nathan Awit <awitnathan@gmail.com>
2023-06-08 15:21:22 +0800

    added unit tests

---
+++ app/src/androidTest/java/com/example/mvopo/tsekapp/InputTextTest.java
@@ -0,0 +1,14 @@
+package com.example.mvopo.tsekapp;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class InputTextTest {
+
+    @Test
+    public void testAdd(){
+        assertEquals(42, Integer.sum(19, 23));
+    }
+
+}
